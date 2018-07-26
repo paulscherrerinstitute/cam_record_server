f2a355ab69e0b5a127aff42f8ed94c46c98d20d3
Andrej Babic <andrej.babic@psi.ch>
2018-07-26 10:57:06 +0200

    Add REST api

---
+++ cam_record_server/rest_api/rest_client.py
@@ -0,0 +1,111 @@
+import requests
+
+from cam_record_server import config
+
+
+def validate_response(server_response):
+    if server_response["state"] != "ok":
+        raise ValueError(server_response.get("status", "Unknown error occurred."))
+
+    return server_response
+
+
+class CamRecordClient(object):
+    def __init__(self, address="http://sf-daqsync-02:%d/" % config.DEFAULT_API_PORT):
+        """
+        :param address: Address of the cam API, e.g. http://localhost:10000
+        """
+
+        self.api_address_format = address.rstrip("/") + config.API_PREFIX + "%s"
+        self.address = address
+
+    def get_rest_address(self):
+        """
+        Return the REST api endpoint address.
+        """
+        return self.address
+
+    def get_camera_list(self):
+        """
+        List existing camera configurations.
+        """
+        rest_endpoint = "/camera"
+
+        server_response = requests.get(self.api_address_format % rest_endpoint).json()
+        return validate_response(server_response)["cameras"]
+
+    def get_camera_config(self, camera_name):
+        """
+        Get the config associated with a camera.
+        :param camera_name: Name of the camera.
+        :return: Config.
+        """
+        rest_endpoint = "/camera/%s" % camera_name
+
+        server_response = requests.get(self.api_address_format % rest_endpoint).json()
+        return validate_response(server_response)["config"]
+
+    def save_camera_config(self, camera_name, configuration):
+        """
+        Save or overwrite camera config.
+        :param camera_name: Camera to save the config to.
+        :param configuration: Config to save, in dictionary format.
+        :return: Saved config.
+        """
+        rest_endpoint = "/camera/%s" % camera_name
+
+        server_response = requests.put(self.api_address_format % rest_endpoint, json=configuration).json()
+        return validate_response(server_response)["config"]
+
+    def delete_camera_config(self, camera_name):
+        """
+        Delete config of camera.
+        :param camera_name: Camera to delete.
+        :return: Deleted config.
+        """
+        rest_endpoint = "/camera/%s" % camera_name
+
+        server_response = requests.delete(self.api_address_format % rest_endpoint).json()
+        return validate_response(server_response)["config"]
+
+    def get_server_info(self):
+        """
+        Return the info of the cam record server instance. For administrative purposes only.
+        :return: Status of the server
+        """
+        rest_endpoint = "/server"
+        server_response = requests.get(self.api_address_format % rest_endpoint).json()
+
+        return validate_response(server_response)["info"]
+
+    def get_camera_info(self, camera_name):
+        """
+        Return the info of the camera instance. For administrative purposes only.
+        :return: Status of the camera.
+        """
+        rest_endpoint = "/server/%s" % camera_name
+        server_response = requests.get(self.api_address_format % rest_endpoint).json()
+
+        return validate_response(server_response)["info"]
+
+    def start_camera(self, camera_name):
+        """
+        Start the camera.
+        :param camera_name: Name of the camera to start.
+        :return: Camera instance info.
+        """
+        rest_endpoint = "/server/%s" % camera_name
+
+        server_response = requests.put(self.api_address_format % rest_endpoint).json()
+        validate_response(server_response)["info"]
+
+    def stop_camera(self, camera_name):
+        """
+        Stop the camera.
+        :param camera_name: Name of the camera to stop.
+        :return: Camera instance info.
+        """
+        rest_endpoint = "/server/%s" % camera_name
+
+        server_response = requests.delete(self.api_address_format % rest_endpoint).json()
+        validate_response(server_response)
---
+++ cam_record_server/rest_api/rest_server.py
@@ -0,0 +1,99 @@
+import json
+import logging
+
+import bottle
+from bottle import request, response
+
+from cam_record_server import config
+
+_logger = logging.getLogger(__name__)
+
+
+def register_rest_interface(app, instance_manager, api_prefix=None):
+    """
+    Get the rest api server.
+    :param app: Bottle app to register the interface to.
+    :param instance_manager: Manager for the server.
+    :param api_prefix: Prefix to put before commands in URL.
+    """
+
+    if api_prefix is None:
+        api_prefix = config.API_PREFIX
+
+    @app.get(api_prefix + "/camera")
+    def get_camera_list():
+        return {"state": "ok",
+                "status": "List of recorded cameras.",
+                "cameras": instance_manager.get_camera_list()}
+
+    @app.get(api_prefix + "/camera/<camera_name>")
+    def get_camera_config(camera_name):
+        return {"state": "ok",
+                "status": "Info for camera %s." % camera_name,
+                "config": instance_manager.get_camera_config(camera_name)}
+
+    @app.put(api_prefix + '/camera/<camera_name>')
+    def save_camera_config(camera_name):
+        return {"state": "ok",
+                "status": "Camera %s configuration saved." % camera_name,
+                "config": instance_manager.save_camera_config(camera_name, request.json)}
+
+    @app.delete(api_prefix + '/camera/<camera_name>')
+    def delete_camera_config(camera_name):
+        return {"state": "ok",
+                "status": "Camera %s configuration deleted." % camera_name,
+                "config": instance_manager.delete_camera_config(camera_name)}
+
+    @app.get(api_prefix + "/server")
+    def get_server_info():
+        return {"state": "ok",
+                "status": "Server info.",
+                "info": instance_manager.get_server_info()}
+
+    @app.get(api_prefix + "/server/<camera_name>")
+    def get_camera_info(camera_name):
+        return {"state": "ok",
+                "status": "Camera %s info." % camera_name,
+                "info": instance_manager.get_camera_info(camera_name)}
+
+    @app.put(api_prefix + '/server/<camera_name>')
+    def start_camera(camera_name):
+        instance_manager.start_camera(camera_name)
+
+        return {"state": "ok",
+                "status": "Camera %s stream started." % camera_name,
+                "info": instance_manager.get_camera_info(camera_name)}
+
+    @app.delete(api_prefix + '/server/<camera_name>')
+    def stop_camera(camera_name):
+        instance_manager.stop_camera(camera_name)
+
+        return {"state": "ok",
+                "status": "Camera %s stream stopped." % camera_name,
+                "info": instance_manager.get_camera_info(camera_name)}
+
+    @app.error(405)
+    def method_not_allowed(res):
+        if request.method == 'OPTIONS':
+            new_res = bottle.HTTPResponse()
+            new_res.set_header('Access-Control-Allow-Origin', '*')
+            new_res.set_header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+            new_res.set_header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type')
+            return new_res
+        res.headers['Allow'] += ', OPTIONS'
+        return request.app.default_error_handler(res)
+
+    @app.hook('after_request')
+    def enable_cors():
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+        response.headers[
+            'Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+    @app.error(500)
+    def error_handler_500(error):
+        response.content_type = 'application/json'
+        response.status = 200
+
+        return json.dumps({"state": "error",
+                           "status": str(error.exception)})
