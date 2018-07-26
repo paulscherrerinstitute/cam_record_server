6b68bdf9839c5063bdd62518c77e1f90c6b932c7
Andrej Babic <andrej.babic@psi.ch>
2018-07-26 12:51:02 +0200

    Add manager stub

--- cam_record_server/manager.py
+++ cam_record_server/manager.py
@@ -2,3 +2,34 @@ class CamRecordInstanceManager(object):
 
     def __init__(self, cam_client):
         self.cam_client = cam_client
+
+    def get_camera_list(self):
+        pass
+
+    def get_camera_config(self, camera_name):
+        pass
+
+    def save_camera_config(self, camera_name, configuration):
+        pass
+
+    def delete_camera_config(self, camera_name):
+        pass
+
+    def get_server_info(self):
+        pass
+
+    def start_all_cameras(self):
+        pass
+
+    def stop_all_cameras(self):
+        pass
+
+    def get_camera_info(self, camera_name):
+        pass
+
+    def start_camera(self, camera_name):
+        pass
+
+    def stop_camera(self, camera_name):
+        pass
+
