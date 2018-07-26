1afe0b8c1bb9b6b8189005886cc4fb31ba33d513
Andrej Babic <andrej.babic@psi.ch>
2018-07-26 12:59:51 +0200

    Add test stubs for config manager

---
+++ tests/test_config_manager.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestConfigManager(unittest.TestCase):
+    def test_basic_operations(self):
+        # TODO: test basic interaction.
+        pass
+
+    def test_config_validation(self):
+        # TODO: Implement.
+        pass
