b6e2b66580a4c860bb512e76c1ac7ff2551a25f6
Andrej Babic <andrej.babic@psi.ch>
2018-07-26 13:26:26 +0200

    Add test stubs for instance manager

---
+++ tests/test_instance_manager.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestInstanceManager(unittest.TestCase):
+    def test_instance_creation(self):
+        # TODO
+        pass
+
+    def test_autostart(self):
+        # TODO
+        pass
+
+    def test_instance_deletion(self):
+        # TODO
+        pass
